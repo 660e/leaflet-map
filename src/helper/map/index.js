995bb990e7a938f807b3959287c1353f7d32c6eb
660e <solunar@vip.qq.com>
2022-06-12 10:38:45 +0800

    update

--- src/helper/map/index.js
+++ src/helper/map/index.js
@@ -2,10 +2,23 @@ import L from 'leaflet';
 
 import 'leaflet/dist/leaflet.css';
 
+const helpers = {
+  initialise(element, center, zoom) {
+    const _ = L.map(element, { center, zoom });
+
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(_);
+
+    return _;
+  }
+};
+
 class Map {
-  constructor(el, center, zoom) {
-    this.map = L.map(el, { center, zoom });
-    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(this.map);
+  constructor(element, center, zoom) {
+    this.map = helpers.initialise(element, center, zoom);
+  }
+
+  version() {
+    return '1.0.0';
   }
 }
 
